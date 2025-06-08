ceabc27427630482ce20972a907fc5941e1d2742
Niraj Kumar <nirajk23523@gmail.com>
2025-06-08 18:08:50 +0530

    feat: made the sign-in ui

---
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -0,0 +1,161 @@
+"use client";
+
+import Link from "next/link";
+import { OctagonAlertIcon } from "lucide-react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(1, "Password is required"),
+});
+
+export const SignInView = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+    return (
+        <div className="flex flex-col gap-6">
+            <Card className="overflow-hidden p-0">
+                <CardContent className="grid p-0 md:grid-cols-2">
+                    <Form {...form}>
+                        <form className="p-6 md:p-8">
+                            <div className="flex flex-col gap-6">
+                                <div className="flex flex-col items-center text-center">
+                                    <h1 className="text-2xl font-bold">
+                                        Welcome Back
+                                    </h1>
+                                    <p className="text-muted-foreground text-balance">
+                                        Login to your account
+                                    </p>
+                                </div>
+
+                                <div className="grid gap-3">
+                                    <FormField
+                                        control={form.control}
+                                        name="email"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Email</FormLabel>
+                                                <FormControl>
+                                                    <Input
+                                                        placeholder="hello@world.com"
+                                                        {...field}
+                                                        type="email"
+                                                    />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+
+                                <div className="grid gap-3">
+                                    <FormField
+                                        control={form.control}
+                                        name="password"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Password</FormLabel>
+                                                <FormControl>
+                                                    <Input
+                                                        placeholder="********"
+                                                        {...field}
+                                                        type="password"
+                                                    />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+
+                                {true && (
+                                    <Alert className="bg-destructive/10 border-none">
+                                        <OctagonAlertIcon className="h-4 w-4 !text-destructive" />
+                                        <AlertTitle>Error</AlertTitle>
+                                    </Alert>
+                                )}
+
+                                <Button type="submit" className="w-full">
+                                    Sign In
+                                </Button>
+
+                                <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                                    <span className="bg-card text-muted-foreground relative z-10 px-2">
+                                        Or continue with
+                                    </span>
+                                </div>
+
+                                <div className="grid grid-cols-2 gap-4">
+                                    <Button
+                                        variant="outline"
+                                        className="w-full"
+                                        type="button"
+                                    >
+                                        Google
+                                    </Button>
+                                    <Button
+                                        variant="outline"
+                                        className="w-full"
+                                        type="button"
+                                    >
+                                        GitHub
+                                    </Button>
+                                </div>
+
+                                <div className="text-center text-sm">
+                                    Don&apos;t have an account?{"  "}
+                                    <Link
+                                        href="/sign-up"
+                                        className="underline underline-offset-4"
+                                    >
+                                        Sign up
+                                    </Link>
+                                </div>
+                            </div>
+                        </form>
+                    </Form>
+
+                    <div className="bg-radial from-green-700 to-green-900 relative md:flex flex-col gay-y-4 items-center justify-center">
+                        <Image
+                            height={92}
+                            width={92}
+                            src="/logo.svg"
+                            alt="Image"
+                            className="h-[92px] w-[92px]"
+                        />
+                        <p className="text-2xl font-semibold text-white">
+                            AigentHub
+                        </p>
+                    </div>
+                </CardContent>
+            </Card>
+
+            <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+                By clicking continue, you agree to our{" "}
+                <a href="#">Terms of Service</a> and{" "}
+                <a href="#">Privacy Policy</a>.
+            </div>
+        </div>
+    );
+};
