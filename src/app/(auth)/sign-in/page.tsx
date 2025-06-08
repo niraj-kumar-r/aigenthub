e8f192724e558f185ecc27a923906092026851a0
Niraj Kumar <nirajk23523@gmail.com>
2025-06-08 16:31:42 +0530

    feat: added common layout to auth

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import { Card } from "@/components/ui/card";
+import React from "react";
+
+const Page = () => {
+    return <Card>Sign-in Page</Card>;
+};
+
+export default Page;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import { Card } from "@/components/ui/card";
+import React from "react";
+
+const Page = () => {
+    return <Card>Sign-up Page</Card>;
+};
+
+export default Page;
