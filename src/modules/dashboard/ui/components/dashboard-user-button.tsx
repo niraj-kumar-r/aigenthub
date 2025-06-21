8b5dbf2ae355c88847dc46ca3e79a72ea149fc4f
Niraj Kumar <nirajk23523@gmail.com>
2025-06-21 21:39:23 +0530

    feat: added dashboard user dropdown options

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -1,12 +1,31 @@
+import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from "lucide-react";
+
+import { GeneratedAvatar } from "@/components/generated-avatar";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import {
     DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
 export const DashboardUserButton = () => {
+    const router = useRouter();
     const { data, isPending } = authClient.useSession();
+
+    const onLogout = () => {
+        authClient.signOut({
+            fetchOptions: {
+                onSuccess: () => {
+                    router.push("/sign-in");
+                },
+            },
+        });
+    };
 
     if (isPending || !data?.user) {
         return null;
@@ -19,8 +38,45 @@ export const DashboardUserButton = () => {
                     <Avatar>
                         <AvatarImage src={data.user.image} />
                     </Avatar>
-                ) : null}
+                ) : (
+                    <GeneratedAvatar
+                        seed={data.user.name}
+                        variant="initials"
+                        className="size-9 mr-3"
+                    />
+                )}
+                <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+                    <p className="text-sm truncate w-full">{data.user.name}</p>
+                    <p className="text-xs truncate w-full">{data.user.email}</p>
+                </div>
+                <ChevronDownIcon className="size-4 shrink-0" />
             </DropdownMenuTrigger>
+
+            <DropdownMenuContent align="end" side="right" className="w-72">
+                <DropdownMenuLabel>
+                    <div className="flex flex-col gap-1">
+                        <span className="font-medium truncate">
+                            {data.user.name}
+                        </span>
+                        <span className="text-sm font-normal text-muted-foreground truncate">
+                            {data.user.email}
+                        </span>
+                    </div>
+                </DropdownMenuLabel>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem className="cursor-pointer flex items-center justify-between">
+                    Billing <CreditCardIcon />
+                </DropdownMenuItem>
+
+                <DropdownMenuItem
+                    onClick={onLogout}
+                    className="cursor-pointer flex items-center justify-between"
+                >
+                    Logout <LogOutIcon />
+                </DropdownMenuItem>
+            </DropdownMenuContent>
         </DropdownMenu>
     );
 };
