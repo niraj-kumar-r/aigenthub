ff9902552b8e6a1ae233d87c9c91a3eee7461e33
Niraj Kumar <nirajk23523@gmail.com>
2025-06-21 21:39:41 +0530

    chore: changed signin and signup page colors

--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -250,7 +250,7 @@ export const SignUpView = () => {
                         </form>
                     </Form>
 
-                    <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+                    <div className="bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 items-center justify-center">
                         <Image
                             height={92}
                             width={92}
