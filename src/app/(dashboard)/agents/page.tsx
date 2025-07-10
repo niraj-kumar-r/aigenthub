8b7966c2463a4332f22bdf0f739af1db67534f1f
Niraj Kumar <nirajk23523@gmail.com>
2025-07-10 23:33:35 +0530

    feat: add error and loading states for agents view

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,7 +1,26 @@
-import { AgentsView } from "@/modules/agents/ui/views/agents-view";
+import {
+    AgentsView,
+    AgentsViewError,
+    AgentsViewLoading,
+} from "@/modules/agents/ui/views/agents-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
-const Agents = () => {
-    return <AgentsView />;
+const Agents = async () => {
+    const queryClient = getQueryClient();
+    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+
+    return (
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Suspense fallback={<AgentsViewLoading />}>
+                <ErrorBoundary fallback={<AgentsViewError />}>
+                    <AgentsView />
+                </ErrorBoundary>
+            </Suspense>
+        </HydrationBoundary>
+    );
 };
 
 export default Agents;
