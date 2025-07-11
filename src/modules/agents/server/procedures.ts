e1f9b1930be17d1a33d5b14c1bf99af3025f47d3
Niraj Kumar <nirajk23523@gmail.com>
2025-07-11 12:40:34 +0530

    feat: implement agentsInsertSchema and protectedProcedure for agent creation

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,10 +1,31 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import {
+    baseProcedure,
+    createTRPCRouter,
+    protectedProcedure,
+} from "@/trpc/init";
+import { agentsInsertSchema } from "../schemas";
 
 export const agentsRouter = createTRPCRouter({
+    // TODO : change to protectedProcedure
     getMany: baseProcedure.query(async () => {
         const data = await db.select().from(agents);
         return data;
     }),
+
+    create: protectedProcedure
+        .input(agentsInsertSchema)
+        .mutation(async ({ input, ctx }) => {
+            // Because drizzle always returns an array of results
+            const [createdAgent] = await db
+                .insert(agents)
+                .values({
+                    ...input,
+                    userId: ctx.auth.user.id,
+                })
+                .returning();
+
+            return createdAgent;
+        }),
 });
