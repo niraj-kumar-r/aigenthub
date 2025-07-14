54306944c99c0435e7453f921913b51f793f8278
Niraj Kumar <nirajk23523@gmail.com>
2025-07-14 16:48:13 +0530

    feat: implement DataTable component and integrate it into AgentsView for displaying agent data (from shadcn docs)

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -4,12 +4,33 @@ import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { columns, Payment } from "../components/columns";
+import { DataTable } from "../components/data-table";
+
+const mockData: Payment[] = [
+    {
+        id: "728ed52f",
+        amount: 100,
+        status: "pending",
+        email: "m@example.com",
+    },
+    {
+        id: "b3c1f4a2",
+        amount: 200,
+        status: "processing",
+        email: "john@john.com",
+    },
+];
 
 export const AgentsView = () => {
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-    return <div>{JSON.stringify(data, null, 2)}</div>;
+    return (
+        <div>
+            <DataTable data={mockData} columns={columns} />
+        </div>
+    );
 };
 
 export const AgentsViewLoading = () => {
